33352731bb9ceb89dd5fbd641682d90d88e25ad5
Anurodh Singh <asingh@isystango.com>
2020-09-11 12:24:01 +0530

    added date time picker component

---
+++ app/components/dateTimePicker/DateTimePickerComponent.js
@@ -0,0 +1,52 @@
+import React, {useState} from 'react';
+import {Button, View, Text, Platform} from 'react-native';
+import styles from './styles';
+import DateTimePicker from '@react-native-community/datetimepicker';
+
+export default function DateTimePickerComponent() {
+  const [date, setDate] = useState(new Date(1598051730000));
+  const [mode, setMode] = useState('date');
+  const [show, setShow] = useState(false);
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setShow(Platform.OS === 'ios');
+    setDate(currentDate);
+  };
+
+  const showMode = currentMode => {
+    setShow(true);
+    setMode(currentMode);
+  };
+
+  const showDatepicker = () => {
+    showMode('date');
+  };
+
+  const showTimepicker = () => {
+    showMode('time');
+  };
+  return (
+    <View style={styles.container}>
+      <Text>Date Time Picker Screen</Text>
+      <View>
+        <View>
+          <Button onPress={showDatepicker} title="Show date picker!" />
+        </View>
+        <View>
+          <Button onPress={showTimepicker} title="Show time picker!" />
+        </View>
+        {show && (
+          <DateTimePicker
+            testID="dateTimePicker"
+            value={date}
+            mode={mode}
+            is24Hour={true}
+            display="default"
+            onChange={onChange}
+          />
+        )}
+      </View>
+    </View>
+  );
+}
